5f2c88ae2c7580fe3b32a25730334618f6a926f5
unknown <visconti.joao@gmail.com>
2023-06-08 10:49:41 +0100

    Create the radio buttons to select register or login

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,4 +1,10 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
+
+enum Auth {
+  login,
+  register,
+}
 
 class AuthScreen extends StatefulWidget {
   static const String routeName = '/auth-screen';
@@ -9,10 +15,59 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.register;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Auth Page')),
-    );
+    return Scaffold(
+        backgroundColor: GlobalVariables.greyBackgroundColor,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const Text(
+                  'Welcome',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                ListTile(
+                  title: const Text(
+                    'Create Account',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  leading: Radio(
+                    activeColor: GlobalVariables.secondaryColor,
+                    value: Auth.register,
+                    groupValue: _auth,
+                    onChanged: (Auth? value) {
+                      setState(() {
+                        _auth = value!;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text(
+                    'Login Account',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  leading: Radio(
+                    activeColor: GlobalVariables.secondaryColor,
+                    value: Auth.login,
+                    groupValue: _auth,
+                    onChanged: (Auth? value) {
+                      setState(() {
+                        _auth = value!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
